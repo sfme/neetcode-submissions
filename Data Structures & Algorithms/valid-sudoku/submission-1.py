43571ab7bc05720a80e73fc2da3c871ae02f387e
Simão Eduardo <simao_eduardo@outlook.com>
2026-05-24 23:51:06 +0100

    Add: valid-sudoku - submission-1

---
+++ Data Structures & Algorithms/valid-sudoku/submission-1.py
@@ -0,0 +1,52 @@
+class Solution:
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        rows_set = [set() for _ in range(9)]
+        cols_set = [set() for _ in range(9)]
+        squares_set = [set() for _ in range(9)]
+
+        # check rows
+        for idx, row in enumerate(board):
+            for elem in row:
+                if elem == '.': # skip empty cells
+                    continue
+
+                if elem in rows_set[idx]:
+                    return False
+                else:
+                    rows_set[idx].add(elem)
+
+        # check cols
+        for idx, col in enumerate(zip(*board)):
+            for elem in col:
+                if elem == '.': # skip empty cells
+                    continue
+
+                if elem in cols_set[idx]:
+                    return False
+                else:
+                    cols_set[idx].add(elem)
+
+        # check squares
+        for ii in range(9): # number of boxes
+            _set_square = squares_set[ii]
+
+            for jj in range(9): # cell inside the box       
+                row = (ii // 3) * 3 + (jj // 3)
+                col = (ii % 3) * 3 + (jj % 3)
+                cell_value = board[row][col]
+
+                if cell_value == '.': # skip empty cells
+                    continue
+
+                if cell_value in _set_square:
+                    return False
+                else:
+                    _set_square.add(cell_value)
+
+        return True
+
+
+
+        
